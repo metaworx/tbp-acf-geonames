442ab4b5243555b2fd607be797c151a41c6bfa79
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2020-09-18 02:28:06 +0200

    [CLEANUP] Whitespace

--- src/Field.php
+++ src/Field.php
@@ -53,8 +53,8 @@ abstract class Field
      *
      *  description
      *
-     * @date      24/10/13
-     * @since     5.0.0
+     * @date         24/10/13
+     * @since        5.0.0
      *
      *
      * @return   void
@@ -138,7 +138,7 @@ abstract class Field
             $myErrorHandler(E_USER_ERROR, $e->getMessage(), $e->getFile(), $e->getLine());
         };
 
-        $oldErrorHandler = set_error_handler($myErrorHandler);
+        $oldErrorHandler     = set_error_handler($myErrorHandler);
         $oldExceptionHandler = set_exception_handler($myExceptionHandler);
 
         // validate ajax request
