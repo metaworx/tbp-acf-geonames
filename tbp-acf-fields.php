9541cffc727b7afefc379e791b8738a083ca7431
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2020-10-21 16:42:33 +0200

    [TASK] Require wp-geoname v3.4.4

--- tbp-acf-fields.php
+++ tbp-acf-fields.php
@@ -47,7 +47,7 @@ call_user_func(
             ],
             'wp-geonames/wp-geonames.php'    => [
                 'name'     => 'WP Geonames',
-                'version'  => '3.4.0',
+                'version'  => '3.4.4',
                 'url'      => 'https://wordpress.org/plugins/wp-geonames/',
                 'required' => false,
                 'fields'   => [
