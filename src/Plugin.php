e8c2a0fc95fd952d8d61edd445d162a93ac3a964
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2020-11-20 23:30:04 +0100

    [TASK] Bump Version to 2.16.1

--- src/Plugin.php
+++ src/Plugin.php
@@ -110,7 +110,7 @@ class Plugin
         // settings
         // - these will be passed into the field class.
         $this->settings = [
-            'version' => '2.16.0',
+            'version' => '2.16.1',
             'url'     => plugin_dir_url( $file ),
             'path'    => plugin_dir_path( $file ),
         ];
