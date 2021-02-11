d4d7f13215b66658a55b36d55adcdf184fb32b22
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2021-02-11 01:40:05 +0100

    [TASK] Bump version to v3.5.2

--- tbp-acf-fields.php
+++ tbp-acf-fields.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields: TBP Collection
 Plugin URI: PLUGIN_URL
 Description: List of worldwide locations based on geonames.org and the geonames plugin
-Version: 3.5.1
+Version: 3.5.2
 Author: Bhujagendra Ishaya, The Bright Path
 Author URI: https://www.thebrightpath.com
 License: GPLv2 or later
