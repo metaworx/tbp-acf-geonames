ab6c47d18ef9d1ebc0e67d43c761b4fd6ff97cef
Bhujagendra Ishaya <bhujagendra@thebrightpath.com>
2021-02-14 15:18:35 +0100

    [TASK] Bump version to v3.8.1

--- src/Plugin.php
+++ src/Plugin.php
@@ -114,7 +114,7 @@ class Plugin
         // settings
         // - these will be passed into the field class.
         $this->settings = [
-            'version' => '3.7.1',
+            'version' => '3.8.1',
             'url'     => plugin_dir_url( $file ),
             'path'    => plugin_dir_path( $file ),
         ];
